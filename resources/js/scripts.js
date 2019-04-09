823863e7d7dfd7532ddc8a98cba9b0349af17ea4
Juldarod <juldarod10@gmail.com>
2019-04-09 16:23:07 -0500

    html and css changes
    pending javascript changes

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,24 +1,46 @@
-//
-const inputEcua = document.getElementById('ecua-o');
-const inputEcuah = document.getElementById('ecuah-o');
-const inputHor = document.getElementById('hor-o');
-const inputEcli = document.getElementById('ecli-o');
-//
-const inputGradHor = document.getElementById('grad-hor-o');
-const inputGradSexa = document.getElementById('grad-sex-o');
-//
-const inputGrados = document.getElementById('grados');
-const inputMinutos = document.getElementById('minutos');
-const inputSegundos = document.getElementById('segundos');
-//
-const inputHoras = document.getElementById('horas');
-const inputMinutosR = document.getElementById('minutos-r');
-const inputSegundosR = document.getElementById('segundos-r');
 
-function clickEcuaOrEcuahInput () {
+
+// Componentes coordenadas origen
+//
+const inputEcuaO = document.getElementById('ecua-o');
+const inputEcuahO = document.getElementById('ecuah-o');
+const inputHorO = document.getElementById('hor-o');
+const inputEcliO = document.getElementById('ecli-o');
+const inputGalacO = document.getElementById('galac-o');
+//
+const inputGradHoraO = document.getElementById('hora-o');
+const inputGradSexaO = document.getElementById('sexa-o');
+//
+const inputGradoHoraOI = document.getElementById('grado-hora-oi');
+const inputMinutoOI = document.getElementById('minuto-oi');
+const inputSegundoOI = document.getElementById('segundos-oi');
+//
+const inputGradoHoraOD = document.getElementById('grado-hora-od');
+const inputMinutoOD = document.getElementById('minuto-od');
+const inputSegundoOD = document.getElementById('segundo-od');
+
+// Componentes coordenadas destino
+//
+const inputEcuaD = document.getElementById('ecua-d');
+const inputEcuahD = document.getElementById('ecuah-d');
+const inputHorD = document.getElementById('hor-d');
+const inputEcliD = document.getElementById('ecli-d');
+//
+const inputGradHoraD = document.getElementById('hora-d');
+const inputGradSexaD = document.getElementById('sexa-d');
+//
+const inputGradoHoraDI = document.getElementById('grado-hora-di');
+const inputMinutoDI = document.getElementById('minuto-di');
+const inputSegundoDI = document.getElementById('segundo-di');
+//
+const inputGradoHoraDD = document.getElementById('grado-hora-dd');
+const inputMinutoDD = document.getElementById('minuto-dd');
+const inputSegundoDD = document.getElementById('segundo-dd');
+
+function clickEcuaOrEcuahOrigen () {
     //
-    inputGradHor.removeAttribute("disabled");
-    inputGradSexa.removeAttribute("disabled");
+    inputGradHorO.removeAttribute("disabled");
+    inputGradSexaO.removeAttribute("disabled");
 
     //
     if (inputGradHor.checked) {
